8d061fd113c3cb4810ea55e3022cbdbc76d2173e
= <=>
2022-09-02 11:59:05 -0400

    CalendarApp agrega ejemplo de uso HasRouter

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 import { store } from "./store"
 
@@ -8,7 +8,10 @@ export const CalendarApp = () => {
     <Provider store={ store }>
         <BrowserRouter>
           <AppRouter />
-      </BrowserRouter>
+        </BrowserRouter>
+        {/* <HashRouter>
+        <AppRouter />
+        </HashRouter> */}
     </Provider>
   )
 }
